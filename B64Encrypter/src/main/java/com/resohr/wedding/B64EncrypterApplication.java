cc33231c184618b6dfe9885baf7934954787571e
Ashish Sahu <its.sahuashish@gmail.com>
2024-12-08 14:10:34 +0530

    B64Encriterclass

---
+++ B64Encrypter/src/main/java/com/resohr/wedding/B64EncrypterApplication.java
@@ -0,0 +1,13 @@
+package com.resohr.wedding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B64EncrypterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B64EncrypterApplication.class, args);
+	}
+
+}
---
+++ B64Encrypter/src/test/java/com/resohr/wedding/B64EncrypterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resohr.wedding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B64EncrypterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
