6050cb6f34fc6a792530b193fe5633850331b1a3
Ashish Sahu <its.sahuashish@gmail.com>
2024-12-08 22:29:24 +0530

    added new project for Base64 Encryption & Decryption for key  values

---
+++ B64EncrypterAndDecrypter/src/main/java/com/resohr/wedding/B64EncrypterAndDecrypterApplication.java
@@ -0,0 +1,13 @@
+package com.resohr.wedding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B64EncrypterAndDecrypterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B64EncrypterAndDecrypterApplication.class, args);
+	}
+
+}
